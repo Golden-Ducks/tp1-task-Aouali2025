9dd2f11dd1d2e90b7282b495acd9bd75838f9faf
Aouali <tangara18aouali@gmail.com>
2026-02-23 20:20:43 +0100

    Update TP1_NLP.py

--- TP1/TP1_NLP.py
+++ TP1/TP1_NLP.py
@@ -2,8 +2,7 @@
 import re
 
 def clean_text(text):
-    text = text.lower()
-    
+    text = text.lower() 
     symbols = ['@', '#', '$', '&', '*', '_', '-', '–']
     for s in symbols:
         text = text.replace(s, '')
@@ -17,38 +16,14 @@ def clean_text(text):
     
     return cleaned
 
-
 with open("task1.txt", "r", encoding="utf-8") as f:
     text = f.read()
-
 cleaned_text = clean_text(text)
-
 print(" Cleaning text :")
 print(cleaned_text)
 
 # TASK 2 :Text normalisation
-
-import re
-
-def number_to_words(n):
-    ones = ["zero","one","two","three","four","five","six","seven","eight","nine"]
-    teens = ["ten","eleven","twelve","thirteen","fourteen","fifteen",
-             "sixteen","seventeen","eighteen","nineteen"]
-    tens = ["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
-    
-    n = int(n)
-    if 0 <= n < 10:
-        return ones[n]
-    elif 10 <= n < 20:
-        return teens[n-10]
-    elif 20 <= n < 100:
-        if n % 10 == 0:
-            return tens[n//10]
-        else:
-            return tens[n//10] + "-" + ones[n%10]
-    else:
-        return str(n) 
-
+from num2words import num2words
 def expand_contractions(text):
     contractions = {
         "i'm": "i am",
@@ -75,23 +50,20 @@ def expand_contractions(text):
         text = re.sub(r"\b{}\b".format(re.escape(c)), contractions[c], text)
     return text
 
-
 def digits_to_words(text):
-    return re.sub(r'\b\d+\b', lambda x: number_to_words(x.group()), text)
+    return re.sub(r'\b\d+\b', lambda x: num2words(int(x.group())), text)
 
 def normalize_text(text):
-    text = text.lower()               
-    text = expand_contractions(text) 
-    text = digits_to_words(text)      
+    text = text.lower()              
+    text = expand_contractions(text)
+    text = digits_to_words(text)     
     cleaned = "".join([c if c.isalnum() or c.isspace() else "" for c in text])
     cleaned = " ".join(cleaned.split())
     return cleaned
-
 
 with open("task2.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 normalized_text = normalize_text(text)
-
 print("Normalized text :")
 print(normalized_text)
